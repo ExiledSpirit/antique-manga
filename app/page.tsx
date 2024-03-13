78ff2beaf23318420a0b3a9bdc7b772454666093
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2024-03-13 12:44:22 -0300

    Corrigindo tamanho máximo da página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { posts } from "@/data/posts";
 
 export default function Home() {
   return (
-    <main className="bg-white w-full h-screen text-black flex flex-col gap-8">
+    <main className="w-full text-black flex flex-col gap-8">
       {
         posts.map((post) => {
           return <PostCard post={post}></PostCard>
