f823826ccf020250745681aed447c5a7810b19c0
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2024-03-14 13:49:48 -0300

    tentativa de gerar rotas build time

--- app/posts/[data]/page.tsx
+++ app/posts/[data]/page.tsx
@@ -7,3 +7,13 @@ export default function PostsPorData({
     </div>
   )
 }
+
+export async function generateStaticParams() {
+  const rotas = [{data: '1'}, {data: '2'}, {data: '3'}]
+ 
+  console.log(rotas);
+
+  return rotas.map((rota) => ({
+    slug: rota.data,
+  }))
+}
